3bb58fb39208e92b9cd65994121aa8a297dbb384
Janaína Bezerra <janainabezerra.ti@gmail.com>
2025-11-06 15:20:00 -0300

    Home e Readme

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Home from "./pages/home/Home";
 
-function App() {
-
+function App () {
   return (
-    <>
-    </>
-  )
+    <Home />
+  );
 }
 
-export default App
+export default App;
